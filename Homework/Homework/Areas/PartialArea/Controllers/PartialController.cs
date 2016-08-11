7872d6b593723817dd58a01934065ab60b49f169
ATai <noreg0379718@gmail.com>
2016-08-11 14:36:02 +0800

    Add AreaTest

---
+++ Homework/Homework/Areas/PartialArea/Controllers/PartialController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework.Models;
+using Homework.Service;
+using Homework.Repositories;
+
+namespace Homework.Areas.PartialArea.Controllers
+{
+    public class PartialController : Controller
+    {
+        private readonly AccountBookService _accServ;
+
+        public PartialController()
+        {
+            var unitOfWork = new EFUnitOfWork();
+            _accServ = new AccountBookService(unitOfWork);
+        }
+
+        // GET: PartialArea/Partial
+        public ActionResult Index()
+        {
+            var result = _accServ.GetAccountingData();
+            return View(result);
+        }
+
+
+    }
+}
---
+++ Homework/Homework/Areas/PartialArea/PartialAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Homework.Areas.PartialArea
+{
+    public class PartialAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PartialArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PartialArea_default",
+                "PartialArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
